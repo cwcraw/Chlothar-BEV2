fe7801b4ce48dd4809e15e927a541ab61fc7e668
cwcraw <cwcraw@gmail.com>
2021-06-30 19:42:14 +0900

    Working on Heroku Playground

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const { ApolloServer } = require('apollo-server');
 const { typeDefs } = require("./typeDefs");
 const { resolvers } = require("./resolvers");
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({ url }) => {
+const server = new ApolloServer({ typeDefs, resolvers,  playground: true});
+const port = process.env.DB_PORT || 4000
+server.listen(port).then(({ url }) => {
   console.log(`🚀  Server ready at ${url} from ${new Date}`);
 });
